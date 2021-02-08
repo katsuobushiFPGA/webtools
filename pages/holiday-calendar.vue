43116fa6e15f8f091a175b9340f4a123792d2c5a
hiroto <suizlet@gmail.com>
2021-02-08 20:19:22 +0900

    [feat] 休日を設定できるUIをつくってみる

---
+++ pages/holiday-calendar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col>
+      オリジナルカレンダー
+      <v-date-picker v-model="selectedDate" readonly :events="holidays" event-color="red" />
+    </v-col>
+    <v-col>
+      休日を選択して反映する
+      <v-date-picker v-model="holidays" multiple />
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      selectedDate: null,
+      holidays: null
+    }
+  }
+}
+</script>
