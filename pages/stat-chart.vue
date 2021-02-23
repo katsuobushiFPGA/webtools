589bfbb004aa99e8585cf0e3190e3e7ef81e158d
hiroto <suizlet@gmail.com>
2021-02-23 17:51:11 +0900

    [fix] pie chart size

--- pages/stat-chart.vue
+++ pages/stat-chart.vue
@@ -5,7 +5,7 @@
     </v-overlay>
     <v-row>
       <v-col class="text-center">
-        <pie-chart />
+        <pie-chart :chart-data="chartData" :width="width" :height="height" />
       </v-col>
     </v-row>
   </v-row>
@@ -16,7 +16,8 @@ import pieChart from '../components/PieChart'
 const VERSION = '3.0'
 const E_STAT_DATA_API_URL = `https://api.e-stat.go.jp/rest/${VERSION}/app/json/getStatsData?`
 export default {
-  components: pieChart,
+  name: 'StatChart',
+  components: { pieChart },
   data() {
     return {
       loading: false,
@@ -31,7 +32,20 @@ export default {
         explanationGetFlg: 'Y',
         annotationGetFlg: 'Y',
         sectionHeaderFlg: '1'
-      }
+      },
+      chartData: {
+        // 凡例とツールチップに表示するラベル
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        // 表示するデータ
+        datasets: [
+          {
+            data: [10, 15, 6, 22, 11, 49, 32],
+            backgroundColor: ['#f87979', '#aa4c8f', '#38b48b', '#006e54', '#c1e4e9', '#89c3eb', '#c3d825']
+          }
+        ]
+      },
+      height: window.innerHeight / 20,
+      width: window.innerWidth / 20
     }
   },
   mounted() {
